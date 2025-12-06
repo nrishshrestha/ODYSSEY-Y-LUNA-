2fbbf4ae9e4ce6388d14e4fe8886008be6d3b6fe
Aavash Dulal <dulalaavas@gmail.com>
2025-12-06 15:37:44 +0545

    DashboardActivity created.

---
+++ app/src/main/java/com/example/odyssey/DashboardActivity.kt
@@ -0,0 +1,35 @@
+package com.example.odyssey
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.odyssey.ui.theme.ODYSSEYTheme
+
+class DashboardActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DashboardBody()
+        }
+    }
+}
+
+@Composable
+fun DashboardBody() {
+
+}
+
+@Preview
+@Composable
+fun DashboardPreview() {
+
+}
