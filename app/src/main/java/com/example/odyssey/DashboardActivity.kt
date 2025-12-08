2ac99de5c4e684532600e4dea49c30a0b5858f55
Aavash Dulal <dulalaavas@gmail.com>
2025-12-08 12:39:46 +0545

    Dashboard topbar added

--- app/src/main/java/com/example/odyssey/DashboardActivity.kt
+++ app/src/main/java/com/example/odyssey/DashboardActivity.kt
@@ -4,29 +4,49 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.odyssey.ui.theme.ODYSSEYTheme
-
 class DashboardActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             DashboardBody()
+
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DashboardBody() {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text(text = "Odyssey")
+                }
+            )
 
+        }
+    ) { padding ->
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(padding))
+        {
+
+        }
+
+    }
 }
+
 
 @Preview
 @Composable
