342761941e43e592086e82161e2840245976d521
Aavash Dulal <dulalaavas@gmail.com>
2025-12-13 22:21:50 +0545

    Made the basic layout for the screen

--- app/src/main/java/com/example/odyssey/HomeScreen.kt
+++ app/src/main/java/com/example/odyssey/HomeScreen.kt
@@ -1,1 +1,40 @@
 package com.example.odyssey
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun HomeScreen() {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFF5F6FA))
+            .padding(20.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+
+    }
+}
+
+
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
+}
